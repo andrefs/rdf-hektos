d22156aa52f7e9daf994932ed9f26847facedcc0
André Santos <andrefs@andrefs.com>
2022-02-08 17:59:13 +0000

    Tests for utils functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import GraphOperations from './lib/GraphOperations.js';
-import flattenObj from './lib/flattenObj.js';
+import {summProps, ppMatrix, flattenObjValues} from './lib/utils.js'
 
 
 import OptionParser from 'option-parser';
@@ -20,49 +20,6 @@ parser.parse();
 if(quiet){ showProgBar = false; }
 
 
-const summarize = (props) => {
-  const res = {};
-  for(const p of Object.keys(props)){
-    res[p] = {};
-    res[p].ratio = props[p].ratio;
-    res[p].count = props[p].count;
-    let len = 0;
-    const walks = {};
-    for(const w of Object.values(props[p].walks)){
-      len += w.nodes.length;
-      walks[w.status] = walks[w.status] || 0;
-      walks[w.status] ++;
-    }
-    res[p].walks = walks;
-
-    const walksCount = Object.keys(props[p].walks).length;
-
-    res[p].avgLen = walksCount ? len/walksCount : 0;
-  }
-  return res;
-}
-
-const prettyPrint = (data) => {
-  const table = [];
-  const keys = {};
-  for(const x of Object.values(data)){
-    for(const k of Object.keys(x)){
-      keys[k] = true;
-    }
-  }
-  table.push(['Property', ...Object.keys(keys)]);
-  for(const [prop,info] of Object.entries(data)){
-    const row = [prop];
-    for(const k of table[0].slice(1)){
-      row.push(info[k] || '');
-    }
-    table.push(row);
-  }
-
-  console.log(table.map(x => x.join('\t')).join('\n'));
-
-};
-
 async function run(){
   console.warn('Starting');
   console.warn('  getting props');
@@ -70,7 +27,7 @@ async function run(){
   await graph.init();
   let props = await graph.getProps();
 
-  const walks = await graph.calcRandomWalks(props, 0.01, 5);
+  const walks = await graph.calcRandomWalks(props, 0.01, 10);
   for(const [p, w] of walks){
     props[p].walks = w;
   }
@@ -85,16 +42,8 @@ async function run(){
   //await calcLoops(props);
 
 
-  const sum  = summarize(props);
-  //const flat = flattenObj(summarize(props));
-  const flat = Object.keys(sum)
-                     .reduce((previous, key) => {
-                       previous[key] = flattenObj(sum[key]);
-                       return previous;
-                     }, {});
-  prettyPrint(flat);
-
-  console.log(JSON.stringify(flat, null, 2));
+  const sum = summProps(props);
+  ppMatrix(flattenObjValues(sum));
 }
 
 run();
