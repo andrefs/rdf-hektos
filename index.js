147765a8c1f884a6f173f6aa777fa6b4e4e9f1c8
André Santos <andrefs@andrefs.com>
2022-02-08 10:36:32 +0000

    More modularity

--- index.js
+++ index.js
@@ -1,177 +1,17 @@
-import Store from './lib/Store.js';
-import {Query, COUNT, URI, RAND} from './lib/QueryBuilder.js';
+//import {Query, COUNT, URI, RAND} from './lib/QueryBuilder.js';
+import {Query, COUNT, V, N, B, L, RAND} from './lib/QueryBuilder.js';
+import GraphOperations from './lib/GraphOperations.js';
 import flattenObj from './lib/flattenObj.js';
-const repo = process.argv[2];
-const host = 'http://localhost';
-const port = '7201';
-const endpointUrl = `${host}:${port}/repositories/${repo}`;
-const store = new Store({endpointUrl});
-import Promise from 'bluebird';
-import cliProgress from 'cli-progress';
-const multibar = new cliProgress.MultiBar({
-    stopOnComplete: true,
-    clearOnComplete: false,
-    hideCursor: true,
-    barSize: 30,
-    format: ' {bar} {percentage}% | {value}/{total} {task} | {tid}'
-}, cliProgress.Presets.shades_grey);
+//const repo = process.argv[2];
+//const host = 'http://localhost';
+//const port = '7201';
+//const endpointUrl = `${host}:${port}/repositories/${repo}`;
+//const store = new Store({endpointUrl});
+import rdf from '@rdfjs/data-model';
+import { SELECT } from '@tpluscode/sparql-builder';
+
 
 let walksBar;
-
-
-
-const s2a = async (stream) => {
-    return new Promise((resolve, reject) => {
-      const res = [];
-      stream
-        .on('data', (bindings) => res.push(bindings))
-        .on('end',() => resolve(res));
-    });
-};
-
-const getProps = async () => {
-  const query = new Query()
-                    .select('p', COUNT('p', 'total'))
-                    .where([['s', 'p', 'o']])
-                    .groupBy('p')
-                    .toSparql();
-  const stream = await store.select(query);
-  const res = await s2a(stream);
-  const props = {};
-  return Object.fromEntries(res.map(r => {
-    return [
-    r.p.value,
-    {
-      count: Number(r.total.value),
-      node: r.p
-    }
-  ];}));
-};
-
-const randomWalks = async (prop, nodes, len) => {
-  //const res = await randomWalk(prop, nodes[0], len, new Set());
-  const walks = {};
-  const pb = multibar.create(nodes.length, 0, {task: 'nodes', tid: ''});
-  const ws = await Promise.map(nodes, n => {
-    pb.increment({task: 'nodes', tid: n.value});
-    return randomWalk(prop, n, len, {});
-  }, {concurrency: 5});
-
-  for (const [i,n] of nodes.entries()){
-    walks[n.value] = ws[i];
-  }
-  multibar.remove(pb);
-  return walks;
-};
-
-
-const randomWalk = async (p, s, len, acc) => {
-  if(len === 0){ return {nodes: Object.keys(acc), status: 'finished'}; }
-
-  const query = new Query()
-                    .select('o')
-                    .where([[s, p, 'o']])
-                    .orderBy(RAND())
-                    .limit(1)
-                    .toSparql();
-
-  const stream =  await store.select(query);
-  const os = await s2a(stream);
-  if(!os.length){
-    return {
-      nodes: Object.keys(acc),
-      status: 'finished_early'
-    };
-  }
-  //const i = Math.floor(Math.random() * os.length);
-  if(os[0].o.termType === 'Literal'){
-    return {nodes: Object.keys(acc), status: 'found_literal'}; r
-  }
-  const o = os[0].o;
-  if(acc[o.value]){
-    return {nodes: Object.keys(acc), status: 'loop'};
-  }
-  acc[s.value] = true;
-
-  return randomWalk(p, o, len-1, acc);
-}
-
-const randSelectSubjects = async (p, howMany) => {
-  const query = new Query()
-                    .select('s')
-                    .where([['s', p, 'o']])
-                    .orderBy(RAND())
-                    .limit(howMany);
-  const q = query.toSparql();
-  const stream = await store.select(q);
-  const subjs = await s2a(stream);
-  return subjs.map(s => s.s);
-}
-
-
-const calcRandomWalks = async (props) => {
-  const pb = multibar.create(Object.keys(props).length, 0, {task: 'props', tid: ''});
-  const pctg = 0.11; // %
-  const walkLength = 10;
-  const walks = [];
-  console.warn(`  doing random walks (${Object.keys(props).length} ` +
-    `props, ${pctg}% of paths, length ${walkLength})`);
-  for (const p of Object.keys(props)){
-    pb.increment({task: 'props', tid: p});
-    const total = props[p].count;
-    const subjs = await randSelectSubjects(props[p].node, Math.ceil(total*pctg/100));
-    const ws = await randomWalks(props[p].node, subjs, walkLength);
-    walks.push([p, ws]);
-  }
-  multibar.remove(pb);
-  return walks;
-};
-
-const calcInOutRatios = async (props) => {
-  const query = `
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-    SELECT ?p (AVG(?c) as ?avg) WHERE {
-        SELECT
-          ?p ?x (COUNT(DISTINCT ?in) as ?cIn)
-          (COUNT(DISTINCT ?out) as ?cOut)
-          (xsd:integer(?cIn)/xsd:integer(?cOut) as ?c) WHERE {
-            ?in ?p ?x .
-            ?x ?p ?out
-        }
-        GROUP BY ?p ?x
-    }
-    GROUP BY ?p
-    ORDER BY DESC(?avg)
-    `;
-
-  const stream = await store.select(query);
-  const res = await s2a(stream);
-  for(const r of res){
-    if(props[r.p.value]){
-      props[r.p.value].ratio = Number(r.avg.value);
-    }
-  }
-
-  return res.map(r => [r.p.value, r.avg.value]);
-};
-
-const calcLoops = async (props) => {
-  const loops = [];
-  const pb = multibar.create(Object.keys(props).length, 0, {task: 'loops', tid: ''});
-  for(const p of Object.keys(props)){
-    const query = `SELECT (COUNT(?s) AS ?loops)
-                   WHERE { ?s <${p}>+ ?s .}`;
-    const stream = await store.select(query);
-    const lc = await s2a(stream);
-    loops.push([p, lc]);
-    pb.increment();
-  }
-  multibar.remove(pb);
-  return loops;
-}
-
-
 
 const summarize = (props) => {
   const res = {};
@@ -219,21 +59,15 @@ const prettyPrint = (data) => {
 async function run(){
   console.warn('Starting');
   console.warn('  getting props');
-  let props = await getProps();
+  const graph = await new GraphOperations('wordnet');
+  let props = await graph.getProps();
 
-  //let p = 'http://www.w3.org/ns/lemon/ontolex#writtenRep';
-  //let p = 'http://www.w3.org/2000/01/rdf-schema#label';
-  //let p = 'http://wordnet-rdf.princeton.edu/ontology#similar';
-  //let p = 'http://www.w3.org/2002/07/owl#sameAs';
-  //props = {[p]: props[p]};
-  //props = Object.fromEntries(Object.entries(props).slice(0,2));
-  
-  const walks = await calcRandomWalks(props);
+  const walks = await graph.calcRandomWalks(props, 0.01, 5);
   for(const [p, w] of walks){
     props[p].walks = w;
   }
 
-  const ratios = await calcInOutRatios(props);
+  const ratios = await graph.calcInOutRatios(props);
   for(const [p, r] of ratios){
     if(props[p]){
       props[p].ratio = r;
