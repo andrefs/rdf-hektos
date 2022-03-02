859b40b5624534eeebd049e0b80f58f9787c94c9
André Santos <andrefs@andrefs.com>
2022-03-02 21:23:21 +0000

    Test GH action

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import GraphOperations from './lib/GraphOperations.js';
 import {summProps, ppMatrix, flattenObjValues} from './lib/utils.js'
 
+//test
 
 import OptionParser from 'option-parser';
 import Store from './lib/Store.js';
