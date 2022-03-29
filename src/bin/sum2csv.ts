604cd63d5443de025087ad26326e9d30b4785b73
André Santos <andrefs@andrefs.com>
2022-03-29 23:09:13 +0100

    script to convert graph info json to csv

---
+++ src/bin/sum2csv.ts
@@ -0,0 +1,18 @@
+import {summPreds, ppMatrix, flattenObjValues} from '../lib/utils'
+import {promises as fs} from 'fs';
+
+import opts from '../lib/opts'
+
+async function run(){
+  console.warn('Starting');
+  const file = opts.input ;
+  console.warn(`Loading file ${file}`);
+  const json = await fs.readFile(file, {encoding: 'utf8'});
+  const {predicates:preds, globalMetrics} = JSON.parse(json);
+
+  const sum = summPreds(preds);
+
+  ppMatrix(flattenObjValues(sum), opts.output);
+}
+
+run();
